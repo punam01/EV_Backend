873a75af5901583bec538e313ac5e95370e08f13
Punam Kumavat <43836363+punam01@users.noreply.github.com>
2024-06-05 22:20:11 +0530

    Update Car.js

--- models/Car.js
+++ models/Car.js
@@ -7,43 +7,56 @@ const carSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     configurations: {
         type: Map,
         of: mongoose.Schema.Types.Mixed,
-        //required: true
     },
     range: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     odometer: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     topSpeed: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     seating: {
         type: Number,
-        required: true
+        required: true,
+        min: 1
     },
     cargoCapacity: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     trim: {
-        type: String,
-        enum: [
-            "RWD",
-            "AWD"
-        ]
+        type: [{
+            name: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true,
+                min: 0
+            }
+        }],
+        validate: [arr => arr.length > 0, 'At least one trim option is required']
     },
     acceleration: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     colors: [{
         color: {
@@ -52,40 +65,50 @@ const carSchema = new mongoose.Schema({
         },
         price: {
             type: Number,
-            required: true
+            required: true,
+            min: 0
         }
     }],
     autoPilot: {
         type: Boolean,
-        required: true
+        default: false
     },
-    wheels: {
-        type: String,
-        enum: [
-            "steel",
-            "alloy",
-            "multiPiece",
-            "chrome",
-            "diamondCut"
-        ]
-    },
+    wheels: [{
+        type: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0
+        }
+    }],
+    steering: [{
+        type: {
+            type: String,
+            enum:[
+                "wheel",
+                "yoke"
+            ],
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0
+        }
+    }],
     images: [
         {
             type: String,
             required: true
         }
     ],
-    steering:{
-        type:String,
-        enum:[
-            "wheel",
-            "yoke"
-        ]
-    },
     testDrive:{
         type:Boolean,
-        required:true
+        default: false
     }
-})
+});
 
-module.exports = mongoose.model('Car', carSchema)
+module.exports = mongoose.model('Car', carSchema);
